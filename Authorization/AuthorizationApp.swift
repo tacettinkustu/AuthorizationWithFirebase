89fd14971a2584d5019677efbc0d896e9d5188f8
Tacettin Kustu <tacettinkustu@gmail.com>
2023-08-13 15:37:55 +0300

    Initial Commit

---
+++ Authorization/AuthorizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthorizationApp.swift
+//  Authorization
+//
+//  Created by Tacettin Küstü on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AuthorizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
